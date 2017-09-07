5eb3654f84b7530917301023209114a6720d2751
Patrick Wilson <patrick.wilson.14@gmail.com>
2017-09-07 10:02:44 -0700

    Update Data-Fetch-Script.js

--- Lab 2/Data-Fetch-Script.js
+++ Lab 2/Data-Fetch-Script.js
@@ -1,8 +1,18 @@
 (function(query) {
 	var results = [];
 	/* Calculate your results here. */
+	var tokenRequest = new sn_ws.RESTMessageV2();
+	tokenRequest.setEndpoint('https://hello.service-now.com/api/snc/spotifytoken');
+	tokenRequest.setHttpMethod('GET');
+	tokenRequest.setRequestHeader("Accept","application/json");
+
+	var tokenResponse = tokenRequest.execute();
+	var tokenBody = new JSON().decode(tokenResponse.getBody());
+	var token = tokenBody.result;
+	
 	var url = "https://api.spotify.com/v1/search?type=artist&q=" + encodeURI(query);
 	var ws = new GlideHTTPRequest(url);
+	ws.addHeader("Authorization", "Bearer " + token);
 	var jsonOutput = ws.get();
 	if (jsonOutput) {
 		var response = new JSON().decode(jsonOutput.getBody());
